eadf341564248035c1c61a24140c9a373fc470b4
qibarli <barliant.rifqi20@gmail.com>
2023-04-30 21:36:03 +0700

    Initial commit

---
+++ TP_MOD10_1302210125/Program.cs
@@ -0,0 +1,10 @@
+﻿using AljabarLibraries;
+
+Double[] akarpersamaankuadrat = AljabarLibraries.QuadraticForm.akarpersamaankuadrat(new double[] { 1, -3, -10 });
+Double[] hasilkuadrat = AljabarLibraries.QuadraticForm.hasilkuadrat(new double[] { 2, -3 });
+
+Console.WriteLine($"Akar Pesamaan Kuadrat X^2-3x-10 = 0 yaitu :\n" +
+    $"X1 = {akarpersamaankuadrat[0]}\n" +
+    $"X2 = {akarpersamaankuadrat[1]}\n\n" +
+    $"Hasil Quadrat dari (2x-3)^2 : {hasilkuadrat[0]}x^2{hasilkuadrat[1]}x+{hasilkuadrat[2]}\n" +
+    $"Hasil Dalam bentuk array : {{{string.Join(", ", hasilkuadrat)}}}");
---
+++ TP_MOD10_1302210125/QuadraticForm.cs
@@ -0,0 +1,23 @@
+﻿namespace AljabarLibraries;
+
+public static class QuadraticForm
+{
+    public static double[] akarpersamaankuadrat(double[] persamaan)
+    {
+        return new double[]
+        {
+            (-persamaan[1] + Math.Sqrt(Math.Pow(persamaan[1],2) - 4*persamaan[0]*persamaan[2])) / (2*persamaan[0]),
+            (-persamaan[1] - Math.Sqrt(Math.Pow(persamaan[1],2) - 4*persamaan[0]*persamaan[2])) / (2*persamaan[0])
+        };
+    }
+
+    public static double[] hasilkuadrat(double[] persamaan)
+    {
+        return new double[]
+        {
+            Math.Pow(persamaan[0],2),
+            persamaan[0] * persamaan[1] * 2,
+            Math.Pow(persamaan[1],2)
+        };
+    }
+}
