570a5ef44b34e4c74a0f0629f4c5869118606890
qibarli <barliant.rifqi20@gmail.com>
2023-04-30 22:34:29 +0700

    Aljabar

--- TP_MOD10_1302210125/Program.cs
+++ TP_MOD10_1302210125/Program.cs
@@ -3,8 +3,8 @@
 Double[] akarpersamaankuadrat = AljabarLibraries.QuadraticForm.akarpersamaankuadrat(new double[] { 1, -3, -10 });
 Double[] hasilkuadrat = AljabarLibraries.QuadraticForm.hasilkuadrat(new double[] { 2, -3 });
 
-Console.WriteLine($"Akar Pesamaan Kuadrat X^2-3x-10 = 0 yaitu :\n" +
+Console.WriteLine($"akar persamaan X^8-11x-2 = 2 yaitu :\n" +
     $"X1 = {akarpersamaankuadrat[0]}\n" +
     $"X2 = {akarpersamaankuadrat[1]}\n\n" +
-    $"Hasil Quadrat dari (2x-3)^2 : {hasilkuadrat[0]}x^2{hasilkuadrat[1]}x+{hasilkuadrat[2]}\n" +
-    $"Hasil Dalam bentuk array : {{{string.Join(", ", hasilkuadrat)}}}");
+    $"hasil quadrat (4x-2)^6 : {hasilkuadrat[0]}x^6{hasilkuadrat[1]}x+{hasilkuadrat[2]}\n" +
+    $"hasil bentuk array : {{{string.Join(", ", hasilkuadrat)}}}");
